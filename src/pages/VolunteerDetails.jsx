3d552b3f785e173df90dfb5fbfd68dfbc25c9783
Rajibbepary <rajib635356@gmail.com>
2024-12-23 20:31:12 +0600

    user email validation

--- src/pages/VolunteerDetails.jsx
+++ src/pages/VolunteerDetails.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios'
-import { format } from 'date-fns'
-import { useEffect, useState } from 'react'
-
+import { compareAsc, format } from 'date-fns'
+import { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../providers/AuthProvider'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { useParams } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 const VolunteerDetails = () => {
+  const { user } = useContext(AuthContext)
   const [startDate, setStartDate] = useState(new Date())
   const { id } = useParams()
   console.log(id)
@@ -20,8 +22,9 @@ const VolunteerDetails = () => {
     const { data } = await axios.get(
       `${import.meta.env.VITE_API_URL}/volunter/${id}`
     )
+
     setVolunter(data)
-    setStartDate(new Date(data.deadline))
+    //setStartDate(new Date(data.deadline))
   }
 
   const {
@@ -34,6 +37,27 @@ const VolunteerDetails = () => {
 volunteersNeeded,
     buyer,
   } = volunter || {}
+
+
+  const handleSubmit= e =>{
+    e.preventDefault()
+    const form = e.target
+    const price = form.price.value
+    const email = user?.email
+    const comment = form.comment.value
+    if(user?.email === buyer?.email) return toast.error('Action not permitted.!')
+
+    if(compareAsc(new Date(), new Date(deadline)) ===1) return toast.error('Deadline Crossed')
+   if(price > volunteersNeeded) return toast.error('Volenteer Not Avilable')
+    if(compareAsc(new Date(startDate), new Date(deadline)) === 1)
+      return toast.error('Offer a date within deadline')
+
+
+    const volunterData = { price, email, comment, deadline}
+    
+  }
+
+
   
   return (
     <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
@@ -89,12 +113,14 @@ volunteersNeeded,
 </div>
         </div>
       </div>
+
+
       <section className='p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]'>
         <h2 className='text-lg font-semibold text-gray-700 capitalize '>
         Volunteer Need
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='price'>
@@ -118,6 +144,7 @@ volunteersNeeded,
                 type='email'
                 name='email'
                 disabled
+                defaultValue={user?.email}
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
             </div>
