5ade23dd03a9b3be8ac5602d73b2ee6f37cf61b2
Rajibbepary <rajib635356@gmail.com>
2025-02-11 12:20:02 +0600

    donation page video add

--- src/components/DonotionVolunter.jsx
+++ src/components/DonotionVolunter.jsx
@@ -1,7 +1,7 @@
 
 import { motion } from 'motion/react';
 import logo from '../assets/images/istockphoto-1498170916-1024x1024.jpg'
-import logo2 from '../assets/images/istockphoto-1498170898-1024x1024.jpg'
+import logo2 from '../assets/images/istockphoto-1077626298-640_adpp_is.mp4'
 const DonotionVolunter = () => {
     return (
         <div className="w-11/12 mx-auto ">
@@ -13,11 +13,24 @@ const DonotionVolunter = () => {
             animate={{ y: [50, 100, 50]}}
             transition={{duration: 10, repeat:Infinity}}
             className='max-w-sm w-64 rounded-t--[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600' src={logo} alt="" />
-            <motion.img
-            animate={{ x: [100, 150, 100]}}
-            transition={{duration: 10, repeat:Infinity}}
-            className='max-w-sm w-64 rounded-t--[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600' src={logo2} alt="" />
+
+
+        <motion.div
+         whileHover={{ scale: 1.1 }}
+          animate={{ x: [100, 150, 100]}}
+         transition={{duration: 10, repeat:Infinity}} >
+        <video
+          src={logo2} // Place your video inside the "public" folder
+          autoPlay
+          loop
+          muted
+          controls
+         className='max-w-sm w-64 rounded-t--[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600'
+        />
+      </motion.div>
+
             </div>
+            
              {/* second div */}
           <div className='flex flex-col gap-5 lg:w-7/12 mt-4'>
             
